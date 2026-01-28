4e03758ccbf46b10116589acf83cf36d54a4ca3f
justkael <144658510+justkael@users.noreply.github.com>
2026-01-29 04:55:18 +0800

    ep 9 done

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,20 +1,15 @@
+@props(['title' => 'demo'])
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title}}</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About us</a>
-        <a href="/contact">Contact us</a>
-    </nav>
-
-    <main>
-    {{ $slot }}
+    <main class="bg-gray-700 p-6 ">
+        {{$slot}}
     </main>
-    
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,60 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Idea;
 
-Route::view('/', 'welcome');
-Route::view('/about', 'about');
-Route::view('/contact', 'contact');
+//index
+Route::get('/ideas', function() {
+    $ideas = Idea::all();   
+
+    return view('ideas/index', [
+        'ideas'=> $ideas,
+    ]);    
+});
+
+//show
+Route::get('/ideas/{idea}', function(Idea $idea) {
+    return view('ideas.show', [
+        'idea'=> $idea,
+    ]);    
+});
+
+//edit
+Route::get('/ideas/{idea}/edit', function(Idea $idea) {
+    return view('ideas.edit', [
+        'idea'=> $idea,
+    ]);    
+});
+
+//update
+Route::patch('/ideas/{idea}', function(Idea $idea) {
+    request()->validate([
+        'description' => 'required|string',
+    ]);
+
+    $idea->update([
+        'description'=>request('description'),
+    ]);  
+
+    return redirect('/ideas/' . $idea->id);
+});
+
+//store
+Route::post('/ideas', function(){
+    $idea = request('description');
+
+    Idea::create([
+        'description' => $idea,
+        'status' => 'pending',
+    ]);
+
+    return redirect('/ideas');
+});  
+
+//destroy
+Route::delete('/ideas/{idea}', function(Idea $idea){
+   $idea->delete();
+
+    return redirect('/ideas');
+});  
+
